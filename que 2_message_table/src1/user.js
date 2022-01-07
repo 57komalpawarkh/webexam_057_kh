76f28ade05ca1c31b33af8ea95a6e46aa5f76f0b
57komalpawarkh <93884562+57komalpawarkh@users.noreply.github.com>
2022-01-07 18:18:02 +0530

    Add files via upload

---
+++ que 2_message_table/src1/user.js
@@ -0,0 +1,40 @@
+const mysql = require('mysql');
+const Promise = require('bluebird');
+Promise.promisifyAll(require("mysql/lib/Connection").prototype);
+
+const dbinfo = {
+    host: "localhost",
+    user: "komal",
+    password: "password",
+    database: "webexam"
+};
+
+const record = {
+    sender: "hitesh",
+    reciever: "amol",
+    msg: "iodex le thoda..."
+}
+
+const addRecord = async (record) => {
+    const connection = mysql.createConnection(dbinfo);
+    await connection.connectAsync();
+    const sql = `insert into message (sender,reciever,msg) values (?,?,?)`;
+    await connection.queryAsync(sql, [record.sender, record.reciever, record.msg]);
+    await connection.endAsync();
+    console.log("message record added..!");
+}
+
+// addRecord(record);
+const getRecord = async () => {
+    const connection = mysql.createConnection(dbinfo);
+    await connection.connectAsync();
+    const sql = `select * from message`;
+    const list = await connection.queryAsync(sql, []);
+    await connection.endAsync();
+    console.log("list of record..!");
+    console.log(list);
+    return list;
+}
+getRecord()
+
+module.exports = { addRecord, getRecord };
